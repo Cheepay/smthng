c95a417065611f55f7caf63f9345c4975463b9c0
Cheepay <40325507+Cheepay@users.noreply.github.com>
2018-06-16 23:24:41 +0200

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#apt update
+#apt upgrade
+#apt install flatpak
+#wget https://flathub.org/repo/appstream/org.gimp.GIMP.flatpakref
+#wget https://flathub.org/repo/appstream/com.valvesoftware.Steam.flatpakref
+#wget https://flathub.org/repo/appstream/com.discordapp.Discord.flatpakref
+#wget https://flathub.org/repo/appstream/org.baedert.corebird.flatpakref
+#flatpak install *.flatpakref
+#wget https://downloads.vivaldi.com/stable/vivaldi-stable_1.15.1147.47-1_amd64.deb
+#wget http://kdl1.cache.wps.com/ksodl/download/linux/a21//wps-office_10.1.0.5707~a21_amd64.deb
+#wget https://download.virtualbox.org/virtualbox/5.2.12/virtualbox-5.2_5.2.12-122591~Ubuntu~bionic_amd64.deb
+#dpkg -i *amd64.deb
+#apt install budgie-desktop
