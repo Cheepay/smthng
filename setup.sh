69756e38815a18e685aefdf87b9b4662e886a6bc
Cheepay <40325507+Cheepay@users.noreply.github.com>
2018-06-22 21:11:10 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -7,7 +7,10 @@
 #wget https://flathub.org/repo/appstream/com.valvesoftware.Steam.flatpakref
 #wget https://flathub.org/repo/appstream/com.discordapp.Discord.flatpakref
 #wget https://flathub.org/repo/appstream/org.baedert.corebird.flatpakref
-#flatpak install *.flatpakref
+#flatpak install org.gimp.GIMP.flatpakref
+#flatpak install com.valvesoftware.Steam.flatpakref
+#flatpak install com.discordapp.Discord.flatpakref
+#flatpak install org.baedert.corebird.flatpakref
 #wget https://downloads.vivaldi.com/stable/vivaldi-stable_1.15.1147.47-1_amd64.deb
 #wget http://kdl1.cache.wps.com/ksodl/download/linux/a21//wps-office_10.1.0.5707~a21_amd64.deb
 #wget https://download.virtualbox.org/virtualbox/5.2.12/virtualbox-5.2_5.2.12-122591~Ubuntu~bionic_amd64.deb
